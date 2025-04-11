1d0c858473db3ec5d45d477611c7434aa5f46d7c
AnooshaMohammadi <anoosha.mohamady@gmail.com>
2025-04-11 17:53:48 +0330

    edit initial_real_population function

--- genetic-1.py
+++ genetic-1.py
@@ -7,5 +7,5 @@ def initial_binary_population(pop_size, chromosome_length):
 
 #creating initial population with real numbers
 def initial_real_population(pop_size, chromosome_length, lower_bound, upper_bound):
-    return np.random.uniform(lower_bound, upper_bound, size=(pop_size, chromosome_length))
-
+    population = np.random.uniform(lower_bound, upper_bound, size=(pop_size, chromosome_length))
+    return np.round(population, 2)
