c49a4272c2bb0efe4e13e79a51de62744196a66d
AnooshaMohammadi <anoosha.mohamady@gmail.com>
2025-04-14 00:52:37 +0330

    add proportional selection

--- genetic-1.py
+++ genetic-1.py
@@ -9,3 +9,13 @@ def initial_binary_population(pop_size, chromosome_length):
 def initial_real_population(pop_size, chromosome_length, lower_bound, upper_bound):
     population = np.random.uniform(lower_bound, upper_bound, size=(pop_size, chromosome_length))
     return np.round(population, 2)
+
+
+def proportional_selection(population, fitness_values, num_parents):
+
+    total_fitness = np.sum(fitness_values)
+    probabilities = fitness_values / total_fitness
+
+    # Choose parents based on probability distribution
+    selected_indices = np.random.choice(len(population), size=num_parents, p=probabilities)
+    return population[selected_indices]
