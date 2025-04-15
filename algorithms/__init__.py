26385e303c92ae64c3823b250da207892c4ddf85
maykhaleghi <maykhaleghi@gmail.com>
2025-04-16 01:31:09 +0330

    caught up to Anoosha's changes for genetic algorithm.

---
+++ algorithms/__init__.py
@@ -0,0 +1,3 @@
+from .genetic_algorithm import initial_binary_population, initial_real_population, proportional_selection, tournament_selection
+
+__all__ = ['initial_binary_population', 'initial_real_population', 'proportional_selection', 'tournament_selection']
---
+++ main.py
@@ -0,0 +1,4 @@
+from algorithms.genetic_algorithm import *
+
+pop = initial_binary_population(10, 10)
+print(pop)
